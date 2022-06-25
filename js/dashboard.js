926d7c73db72bbcd49b728d433b4b7f390171fd9
legazy33 <40193720+legazy33@users.noreply.github.com>
2022-06-25 15:33:04 -0700

    update: edited table and added API request for searched symbol

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,15 +6,38 @@ const searchBar = document.querySelector('.search-input');
 searchBar.addEventListener('keypress', function(e){
     if(e.keyCode == 13){
         const symbol = searchBar.value;
+
+        fetch(`https://rapidapi.p.rapidapi.com/stock/v2/get-summary?symbol=${symbol}&region=US`, {
+	        "method": "GET",
+	        "headers": {
+		        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		        "x-rapidapi-key": "675e232180msh684db82c43224d9p122709jsne5c627d38df4"
+	        }
+            })
+            .then(response => {
+	         return response.json();
+            })
+            .then(data => {
+
+                console.log(data);
+                
+                // var li = document.createElement('p');
+                // li.textContent = `${data.symbol} --- ${data.price.regularMarketPrice.raw}  `;
+                // li.classList.add('portListing');
+                // watchList.appendChild(li);
+            })
+            .catch(err => {
+                console.error('API call not responding please refresh.....');
+            }); 
         
     };
 
-   
-
 })
 
+/////////////////////////
+//  add chart to page //
+////////////////////////
 
-//  add chart to page 
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
